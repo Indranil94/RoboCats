67b07173f7fbd88aebeacfdd9edc6dc6cee2519f
Indranil94 <indra.santra@gmail.com>
2018-11-24 17:50:47 +0530

    formatting

--- src/robocard.js
+++ src/robocard.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const RoboCard = ({id, name, email}) => {
 	return(
-			<div className = "cf bg-light-blue tc ma4 pa3 w-20 br3 b--black-10 fl ">
-				<img src = {`https://robohash.org/set_set4/${id}?size=200x200`} alt = "roboface" className= " br-100 h4 w4 dib ba b--black-05 pa2 grow"/>
+			<div className = "cf bg-light-blue tc ma4 ml5 pa3 br3 b--black-10 w5 fl">
+				<img src = {`https://robohash.org/set_set4/${id}?size=200x200`} alt = "roboface" className= "br-100 h4 w4 dib ba b--black-05 pa2 grow"/>
 				<h3 className = "f4 mb2">{name} </h3>
 				<h3 className = "f5 mb2">{email}</h3>
 			</div>
@@ -11,4 +11,3 @@ const RoboCard = ({id, name, email}) => {
 }
 
 export default RoboCard;
-//mw5 overflow-hidden pa4 ma4 
--- src/scroll.js
+++ src/scroll.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Scroll = (props)=>{
 	return(
-		<div style={{overflowY: "scroll", border: "2px solid black" , height: "33.5em"}}>
+		<div className="pv4 ph2 tc-l" style={{overflowY: "scroll", borderTop: "2px solid black" , height: "35.5em"}}>
 			{props.children}
 		</div>
 		)
