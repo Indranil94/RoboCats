ae7782ba0dcd0a4fe0a2b246aabd28d1afa05eb7
Indranil94 <indra.santra@gmail.com>
2018-11-24 23:28:26 +0530

    manifest

--- src/searchbar.js
+++ src/searchbar.js
@@ -9,7 +9,7 @@ import React from 'react';
 const SearchBar = ({onSearchChange, onSearchClick, onResetClick, textval})=>{
 	return(
 		<div className="tc">
-	        <input className="input-reset bn black-80 bg-lightest-blue pa2 w-15 br2" onChange={onSearchChange} placeholder="Enter Search Term" value={textval} type="text" name="search" id="search" />
+	        <input className="input-reset bn black-80 bg-lightest-blue pa2 w-15 br2" aria-label="Search box" onChange={onSearchChange} placeholder="Enter Search Term" value={textval} type="text" name="search" id="search" />
       		{/*<input className="pa2 button-reset tc bn bg-animate bg-black-70 hover-bg-black white pointer w-10 br2" onClick={onSearchClick} type="submit" value="Search" />*/}
       		<input className="pa2 button-reset tc bn bg-animate bg-black-70 hover-bg-black white pointer w-10 br2" onClick={onResetClick} type="submit" value="Reset" />
       	</div>
