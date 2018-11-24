91d586c69cead1c686ca9aface13b7151d370279
Indranil94 <indra.santra@gmail.com>
2018-11-24 14:16:37 +0530

    Updates

---
+++ precache-manifest.35d354eefb7d9285e2c11fde73ba5613.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2942658854a2985f02e4",
+    "url": "/RoboCats/static/js/runtime~main.29426588.js"
+  },
+  {
+    "revision": "ec7a5f996b9fab642fd8",
+    "url": "/RoboCats/static/js/main.ec7a5f99.chunk.js"
+  },
+  {
+    "revision": "51efa5027252817a79a9",
+    "url": "/RoboCats/static/js/1.51efa502.chunk.js"
+  },
+  {
+    "revision": "ec7a5f996b9fab642fd8",
+    "url": "/RoboCats/static/css/main.a3be9d70.chunk.css"
+  },
+  {
+    "revision": "51efa5027252817a79a9",
+    "url": "/RoboCats/static/css/1.b4ff810a.chunk.css"
+  },
+  {
+    "revision": "edc4554b93a69ab29ee3a0ccf89cb00f",
+    "url": "/RoboCats/index.html"
+  }
+];
